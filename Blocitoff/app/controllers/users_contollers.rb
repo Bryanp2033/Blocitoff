2aa42fbd2c6e98f1f9e14031fb0b325117bb96d0
Bryan <bryanporras2033@gmail.com>
2015-10-17 20:31:19 -0400

    user_sign_up

--- Blocitoff/app/controllers/users_contollers.rb
+++ Blocitoff/app/controllers/users_contollers.rb
@@ -1,1 +1,31 @@
-users_contollers.rb
+class UsersController < ApplicationController
+
+
+	def new
+		@user = User.new
+	end
+
+	
+
+def show
+    @user = User.find(params[:id])
+    
+  end
+  
+   def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated"
+      redirect_to edit_user_registration_path
+    else
+      flash[:error] = "Invalid user information"
+      redirect_to edit_user_registration_path
+    end
+  end
+
+ 
+   private
+ 
+   def user_params
+     params.require(:user).permit(:name, :avatar)
+   end
+ end
