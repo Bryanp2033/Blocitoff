b55411642b17fc70b29e2908fd801d97df4287e1
Bryan <bryanporras2033@gmail.com>
2015-10-27 14:56:53 -0400

    Create to_do_items complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,23 @@
 class ItemsController < ApplicationController
 
   def create
-  	@items = Item.new(params[:id])
-  	if @items.save
+  	@item = current_user.items.build(item_params)
+  	if @item.save
   		flash[:notice] = "Item was saved"
-  		redirect_to 'welcome#index'
+  		redirect_to user_path(current_user)
   	else
   		flash[:error] = "There was an error saving the item. Please try again"
   		render :new
   		
   	end
+  end
+
+  def new
+    @item = Item.new
+end
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
   end
 end
