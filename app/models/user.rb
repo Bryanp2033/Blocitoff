f8ed0c7d1a4370e6b88548b9d8801e24bbc9267c
Bryan <bryanporras2033@gmail.com>
2015-10-29 21:50:04 -0400

    latest changes to ajax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-	has_many :items
+	has_many :items, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+
+        
 
          def days_left
          	7 - (DateTime.now.to_date - created_at.to_date).to_i
